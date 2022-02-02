c3831a10c88f6a8970b51526efb30d57b269416f
gbernal <g.bernalr.2019@alumnos.urjc.es>
2022-02-02 19:20:24 +0100

    Estructura del programa

---
+++ follow_wall/src/follow_wall_node.cpp
@@ -0,0 +1,11 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/LaserScan.hpp"
+
+using namespace std::chrono_literals;
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  return 0;
+}
