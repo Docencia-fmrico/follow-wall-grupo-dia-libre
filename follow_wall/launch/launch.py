da62f0d42f40f55da2a01915f813b52f0499a23c
Chuismiguel <luismilopez35@gmail.com>
2022-02-12 17:25:58 +0100

    launcher should pass tests

--- follow_wall/launch/launch.py
+++ follow_wall/launch/launch.py
@@ -3,8 +3,8 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 from launch.actions import (IncludeLaunchDescription, SetEnvironmentVariable)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
