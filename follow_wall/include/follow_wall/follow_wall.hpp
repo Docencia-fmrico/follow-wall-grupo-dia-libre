a9938a2a2ae0ff2c83a444e17186d0810c9fed1d
Chuismiguel <luismilopez35@gmail.com>
2022-02-12 16:56:47 +0100

    ALL TEST PASSED

--- follow_wall/include/follow_wall/follow_wall.hpp
+++ follow_wall/include/follow_wall/follow_wall.hpp
@@ -35,78 +35,70 @@ using CallbackReturnT = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterfac
 
 class FollowWallLifeCycle : public rclcpp_lifecycle::LifecycleNode
 {
-    public:
-        FollowWallLifeCycle();
+public:
+  FollowWallLifeCycle();
 
-        CallbackReturnT on_configure(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_configure(const rclcpp_lifecycle::State & state);
 
-        CallbackReturnT on_activate(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_activate(const rclcpp_lifecycle::State & state);
 
-        CallbackReturnT on_deactivate(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_deactivate(const rclcpp_lifecycle::State & state);
 
-        CallbackReturnT on_cleanup(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_cleanup(const rclcpp_lifecycle::State & state);
 
-        CallbackReturnT on_shutdown(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_shutdown(const rclcpp_lifecycle::State & state);
 
-        CallbackReturnT on_error(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_error(const rclcpp_lifecycle::State & state);
 
-        void do_work();
+  void do_work();
 
+protected:
+  float distance_to_left_;
+  float distance_to_center_;
+  float distance_max_range_;
+  float distance_upleft_;
+  float prev_error_;
 
-    protected:
-        float distance_to_left_;
-        float distance_to_center_;
-        float distance_max_range_;
-        float distance_upleft_;
-        float prev_error_;
+private:
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+  rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::Twist>::SharedPtr speed_pub_;
+  rclcpp::Time last_time_;
 
-    private:
-        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
-        rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::Twist>::SharedPtr speed_pub_;
-        rclcpp::Time last_time_;
+  int state_;
+  int is_turning_;
+  int turn_to_;
+  float tend_mean_;
+  float prev_mean_;
+  int tend_it_;
+  float min_dist_;
+  int count_it_trend_;
+  int count_it_rot_;
 
-        int state_;
-        int is_turning_;
-        int turn_to_;
-        float tend_mean_;
-        float prev_mean_;
-        int tend_it_;
-        float min_dist_;
-        int count_it_trend_;
-        int count_it_rot_;
+  const float MAX_DIST_RANGE = 25.0;
+  const float TREND_MAX_DIST = 2.0;
+  const float TREND_MIN_DIST = 0.1;
+  const float DIST_VARIATION = 0.05;
+  const int MAX_RECALCULATIONS = 5;
+  const int MAX_ROTATIONS = 80;
+  const float FLOAT_ZERO = 0.0;
+  const float LINEAR_SPEED = 0.25;
+  const float ANGULAR_KD = 1 / 10;
+  const float SWEEPING_RANGE = 6;
+  const float OBJECT_LIMIT = 0.45;
+  const int MAX_IT = 15;
+  const float LEFT_DETECTION_ANGLE = 1.57;
 
-        const float MAX_DIST_RANGE = 25.0;
-        const float TREND_MAX_DIST = 2.0;
-        const float TREND_MIN_DIST = 0.1;
+  // to understand how these functions work check this image
+  // https://imgur.com/a/6N0uFbl
 
-        const float DIST_VARIATION = 0.05;
+  geometry_msgs::msg::Twist turn(int direction, float wvel);
+  bool trend_algortihm(float dist);
+  float get_left_lecture(sensor_msgs::msg::LaserScan::SharedPtr laser_data);
+  float get_object_center(sensor_msgs::msg::LaserScan::SharedPtr laser_data);
+  float get_object_left(sensor_msgs::msg::LaserScan::SharedPtr laser_data);
+  float get_object_upleft(sensor_msgs::msg::LaserScan::SharedPtr laser_data);
 
-        const int MAX_RECALCULATIONS = 5;
-        const int MAX_ROTATIONS = 80;
-
-        const float FLOAT_ZERO = 0.0;
-
-        const float LINEAR_SPEED = 0.25;
-
-        const float ANGULAR_KD = 1/10;
-
-        const float SWEEPING_RANGE = 6;
-        const float OBJECT_LIMIT = 0.45;
-        const int MAX_IT = 15;
-
-        const float LEFT_DETECTION_ANGLE = 1.57;
-
-        // to understand how these functions work check this image
-        // https://imgur.com/a/6N0uFbl
-
-        geometry_msgs::msg::Twist turn(int direction, float wvel);
-        bool trend_algortihm(float dist);
-        float get_left_lecture(sensor_msgs::msg::LaserScan::SharedPtr laser_data);
-        float get_object_center(sensor_msgs::msg::LaserScan::SharedPtr laser_data);
-        float get_object_left(sensor_msgs::msg::LaserScan::SharedPtr laser_data);
-        float get_object_upleft(sensor_msgs::msg::LaserScan::SharedPtr laser_data);
-
-        void laser_cb(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  void laser_cb(const sensor_msgs::msg::LaserScan::SharedPtr msg);
 };
 }  // namespace follow_wall
 
