4b07b181970b1d8dfa110b8513e4e1b7bb3b26e6
Chuismiguel <luismilopez35@gmail.com>
2022-02-12 17:32:07 +0100

    another change to launcher

--- follow_wall/launch/launch.py
+++ follow_wall/launch/launch.py
@@ -4,10 +4,9 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
 from launch.actions import (IncludeLaunchDescription, SetEnvironmentVariable)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch_ros.actions import Node
 
 def generate_launch_description():
     tiago_gazebo_dir = get_package_share_directory('tiago_gazebo')
